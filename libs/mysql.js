b4e874336d2062bfb11162f7189847857fba8bb5
Cristian <dcris3344@gmail.com>
2024-12-10 09:48:01 -0500

    Agregado variables de base de datos

--- libs/mysql.js
+++ libs/mysql.js
@@ -1,8 +1,8 @@
 import mysql from "mysql2/promise";
 
 export const myConexion = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "pampas_travel",
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
 });
