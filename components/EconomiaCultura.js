4e64838b3468a93840e34d61016e13555c95d1c3
dCris96 <dcris3344@gmail.com>
2026-06-06 19:06:44 -0500

    Ajustando el texto del main

--- components/EconomiaCultura.js
+++ components/EconomiaCultura.js
@@ -8,15 +8,15 @@ export default function EconomiaCultura(){
                 <h6 className="economia-subtitulo">Actividades Productivas</h6>
                 <p>La economía del distrito se sustenta en tre ejes principales</p>
                 <ul>
-                    <li><strong>Agricultura:</strong> Cultivos de altura como papa, trigo, cebada y maiz.</li>
-                    <li><strong>Ganadería:</strong> Crianza de ganado vacuno y ovino.</li>
-                    <li><strong>Minería:</strong> Actividad extractiva polimetálica en las zonas altes del distrito.</li>
+                    <li><span className="negrita">Agricultura:</span> Cultivos de altura como papa, trigo, cebada y maiz.</li>
+                    <li><span className="negrita">Ganadería:</span> Crianza de ganado vacuno y ovino.</li>
+                    <li><span className="negrita">Minería:</span> Actividad extractiva polimetálica en las zonas altes del distrito.</li>
                 </ul>
             </div>
             <div className="cultura-card">
                 <div>
                     <h5 className="economia-titulo">🧡 Cultura y Fe</h5>
-                    <p>La festividad más importante es la celebración en honor a <strong>San Agustín</strong> y <strong>Santa Rosa</strong>, la cual se lleva a cabo a finales del mes de Agosto.</p>
+                    <p>La festividad más importante es la celebración en honor a <span className="negrita">San Agustín</span> y <span className="negrita">Santa Rosa</span>, la cual se lleva a cabo a finales del mes de Agosto.</p>
                     <br/>
                     <p>Es el evento cultural mas relevante, donde se manifiesta el sincretismo religioso y el retorno de la diáspora pampasina.</p>
                 </div>
